6df2c788db6acc255dc6e17971d658b83590cd3f
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-07 09:23:54 +0530

    first commit

---
+++ Array/easy/Movezeroes.java
@@ -0,0 +1,13 @@
+public class Movezeroes {
+    public void moveZeroes(int[] nums) {
+        int lastNonZeroIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroIndex++] = nums[i];
+            }
+        }
+        for (int i = lastNonZeroIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
