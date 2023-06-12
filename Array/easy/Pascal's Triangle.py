87dae9230c19a49c717a9a80ea020be9d6c07376
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-12 21:29:42 +0530

    9th commit

--- Array/easy/Pascal's Triangle.py
+++ Array/easy/Pascal's Triangle.py
@@ -1,7 +1,50 @@
-class solution:
-    def gennerate(self,numRow:int) -> List[List[int]]:
-        pascal = [[1]*i for i in range(1,numRow)]
-        for i in range(2,numRow):
+# 118.
+# Pascal
+# 's Triangle
+# Easy
+# 10.2
+# K
+# 328
+# Companies
+# Given
+# an
+# integer
+# numRows,
+# return the
+# first
+# numRows
+# of
+# Pascal
+# 's triangle.
+#
+# In
+# Pascal
+# 's triangle, each number is the sum of the two numbers directly above it as shown:
+#
+# Example
+# 1:
+#
+# Input: numRows = 5
+# Output: [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+# Example
+# 2:
+#
+# Input: numRows = 1
+# Output: [[1]]
+#
+# Constraints:
+#
+# 1 <= numRows <= 30
+
+
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ans =[[1]]
+        for i in range(1,numRows):
+            ans.append([1])
             for j in range(1,i):
-                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
-        return pascal
+                ans[i].append(ans[i-1][j]+ans[i-1][j-1])
+            ans[i].append(1)
+        return ans
