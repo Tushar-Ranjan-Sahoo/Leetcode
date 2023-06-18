ebbcabc0aae9f0429732dd02db8c3d9015969c67
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-18 22:03:54 +0530

    15th

--- Array/Medium/Containerwithmostwater.java
+++ Array/Medium/Containerwithmostwater.java
@@ -31,5 +31,25 @@
 //        2 <= n <= 105
 //        0 <= height[i] <= 104
 public class Containerwithmostwater {
+    public int maxArea(int[] height) {
+        int left=0,right=height.length-1,max=0;
+        while(left<right){
+            max=Math.max(max,Math.min(height[left],height[right])*(right-left));
+            if(height[left]<height[right]){
+                int preleft=height[left];
+                while(height[left]<=preleft && left<right){
+                    left++;
+                }
+            }
+            else{
+                int preright=height[right];
+                while(height[right]<=preright && left<right){
+                    right--;
+                }
+            }
+        }
+        return max;
+    }
+}
 
 }
