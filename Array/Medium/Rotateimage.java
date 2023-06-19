1bfcaef05f06f463ae3adf416b72877cecf88576
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-19 23:29:33 +0530

    16th

--- Array/Medium/Rotateimage.java
+++ Array/Medium/Rotateimage.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Rotateimage {
+public class Rotateimage {
+    int n = matrix.size();
+        for ( int i =0; i<=n ; i++){
+        for (int j = i+1; j<n ; j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+        for(int i =0; i<n;i++){
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
 }
--- Array/Medium/Wordsearch.java
+++ Array/Medium/Wordsearch.java
@@ -1,2 +1,102 @@
-package PACKAGE_NAME;public class Wordsearch {
+//79. Word Search
+//        Medium
+//        13.6K
+//        548
+//        Companies
+//        Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+//
+//        The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+//
+//
+//
+//        Example 1:
+//
+//
+//        Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+//        Output: true
+//        Example 2:
+//
+//
+//        Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+//        Output: true
+//        Example 3:
+//
+//
+//        Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+//        Output: false
+//
+//
+//        Constraints:
+//
+//        m == board.length
+//        n = board[i].length
+//        1 <= m, n <= 6
+//        1 <= word.length <= 15
+//        board and word consists of only lowercase and uppercase English letters.
+
+
+public class Wordsearch {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length, n = board[0].length;
+        if (m*n < word.length())
+            return false;
+        char[] wrd = word.toCharArray();
+        int[] boardf = new int[128];
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                ++boardf[board[i][j]];
+            }
+        }
+        for (char ch : wrd)
+        {
+            if (--boardf[ch] < 0)
+            {
+                return false;
+            }
+        }
+        if (boardf[wrd[0]] > boardf[wrd[wrd.length - 1]])
+            reverse(wrd);
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (wrd[0] == board[i][j]
+                        && found(board, i, j, wrd, new boolean[m][n], 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private void reverse(char[] word)
+    {
+        int n = word.length;
+        for (int i = 0; i < n/2; ++i)
+        {
+            char temp = word[i];
+            word[i] = word[n - i - 1];
+            word[n - i - 1] = temp;
+        }
+    }
+    private static final int[] dirs = {0, -1, 0, 1, 0};
+    private boolean found(char[][] board, int row, int col, char[] word,
+                          boolean[][] visited, int index)
+    {
+        if (index == word.length)
+            return true;
+        if (row < 0 || col < 0 || row == board.length || col == board[0].length
+                || board[row][col] != word[index] || visited[row][col])
+            return false;
+        visited[row][col] = true;
+        for (int i = 0; i < 4; ++i)
+        {
+            if (found(board, row + dirs[i], col + dirs[i + 1],
+                    word, visited, index + 1))
+                return true;
+        }
+        visited[row][col] = false;
+        return false;
+    }
 }
