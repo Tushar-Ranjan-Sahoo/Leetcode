0584362580fb0d950fa53406ebd8a67cc4d61409
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-20 23:09:45 +0530

    17th

--- Array/Medium/threesumclosest.java
+++ Array/Medium/threesumclosest.java
@@ -32,4 +32,27 @@
 
 
 public class threesumclosest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        var closest = 0;
+
+        for (int i =0, n = nums.length, minDiff = Integer.MAX_VALUE; i< n-2 ; i++){
+            for(int j = i+1, k = n- 1 ; j<k;){
+                var sum = nums[i]+ nums[j]+ nums[k];
+                if (sum == target)
+                    return target;
+                else if ( sum<target)
+                    j++;
+                else
+                    k--;
+                var diff = Math.abs(target - sum);
+                if (diff < minDiff){
+                    minDiff = diff;
+                    closest = sum;
+
+                }
+            }
+        }
+        return closest;
+    }
 }
