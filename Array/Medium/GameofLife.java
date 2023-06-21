a9f91ffc69ea561c59651f8876659c0900101e9d
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-21 13:16:01 +0530

    18th commit

--- Array/Medium/GameofLife.java
+++ Array/Medium/GameofLife.java
@@ -83,3 +83,27 @@ public class GameofLife {
     }
 
 }
+//    The gameOfLife method takes in a 2D array of integers called board representing the current state of the game. The method updates the board array in place to represent the next state of the game according to the four rules of the Game of Life.
+//
+//        First, the method gets the dimensions of the board array and creates a new 2D array called nextBoard with the same dimensions. This new array will be used to store the next state of the game.
+//
+//        Next, the method iterates over each cell in the board array using a nested for loop. For each cell, it calls the countLiveNeighbors method to count the number of live neighbors that cell has.
+//
+//        If the current cell is alive (represented by a 1 in the board array), then it applies the first three rules of the Game of Life:
+//
+//        If the cell has fewer than two live neighbors, it dies (represented by a 0 in the nextBoard array).
+//        If the cell has two or three live neighbors, it lives on to the next generation (represented by a 1 in the nextBoard array).
+//        If the cell has more than three live neighbors, it dies (represented by a 0 in the nextBoard array).
+//        If the current cell is dead (represented by a 0 in the board array), then it applies the fourth rule of the Game of Life: 4. If a dead cell has exactly three live neighbors, it becomes a live cell (represented by a 1 in the nextBoard array).
+//
+//        After all cells have been processed and their next state has been determined, the method copies the values from nextBoard back to board.
+//
+//        The countLiveNeighbors method takes in a 2D array called board, and two integers called row and col, representing a specific cell in that board. The method returns an integer representing how many live neighbors that cell has.
+//
+//        First, it initializes a variable called count to 0. This variable will be used to keep track of how many live neighbors have been found.
+//
+//        Next, it iterates over all cells that are adjacent to or diagonal from the specified cell using a nested for loop. For each neighboring cell, it checks if that cell is within bounds and if it is not equal to itself. If both conditions are met, then it adds its value to count. Since live cells are represented by 1s and dead cells are represented by 0s, this effectively counts how many live neighbors there are.
+//
+//        Finally, it returns count.
+//
+//        I hope this explanation helps you understand how this implementation of Game of Life works!
