d7f26cc7c3e4a2a9765d04fafd5ff614a176e6a1
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-22 23:13:28 +0530

    19bth commit

--- Array/Medium/PairsofsongsWithTotalDurationsDivisibleby60.java
+++ Array/Medium/PairsofsongsWithTotalDurationsDivisibleby60.java
@@ -1,2 +1,14 @@
 public class PairsofsongsWithTotalDurationsDivisibleby60 {
+    ublic int numPairsDivisibleBy60(int[] time) {
+        int count = 0;
+        int[] remainders = new int [60];
+        for ( int t :time){
+            int remainder = t%60;
+            int complement = (60-remainder)% 60;
+            count += remainders[complement];
+            remainders[remainder]++;
+
+        }
+        return count;
+    }
 }
