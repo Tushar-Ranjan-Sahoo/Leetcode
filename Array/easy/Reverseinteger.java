ec2822b02142c8c84428c4284eac2792c8aa96d9
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-26 22:19:20 +0530

    22ansth commit

--- Array/easy/Reverseinteger.java
+++ Array/easy/Reverseinteger.java
@@ -27,4 +27,16 @@
 //
 //        -231 <= x <= 231 - 1
 public class Reverseinteger {
+    public int reverse(int x) {
+        int prevRev = 0 , rev= 0;
+        while( x != 0){
+            rev= rev*10 + x % 10;
+            if((rev - x % 10) / 10 != prevRev){
+                return 0;
+            }
+            prevRev = rev;
+            x= x/10;
+        }
+        return rev;
+    }
 }
