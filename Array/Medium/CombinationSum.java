a1c04a029f62ddc40b95c1170af76a084d2aea23
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-27 23:26:22 +0530

    23ansth commit

--- Array/Medium/CombinationSum.java
+++ Array/Medium/CombinationSum.java
@@ -39,4 +39,15 @@ public class CombinationSum {
 
     }
     private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+        if(remain < 0 ) return ;
+        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        else{
+            for(int i = start; i<nums.length; i++){
+                tempList.add(nums[i]);
+                backtrack(list, tempList,nums,remain-nums[i],i);
+                tempList.remove(tempList.size()-1);
+            }
+        }
+    }
+   
 }
