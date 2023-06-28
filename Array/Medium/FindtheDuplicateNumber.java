afcd3bf6f4ac1f9ea500a0083e2fba6b930879a2
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-28 23:01:39 +0530

    24ansth commit

--- Array/Medium/FindtheDuplicateNumber.java
+++ Array/Medium/FindtheDuplicateNumber.java
@@ -1,2 +1,12 @@
+287. Find the Duplicate Number
+        Medium
+        19.7K
+        3K
+        Companies
+        Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+        There is only one repeated number in nums, return this repeated number.
+
+        You must solve the problem without modifying the array nums and uses only constant extra space.
 public class FindtheDuplicateNumber {
 }
