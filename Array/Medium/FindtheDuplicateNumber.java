610c95464ca3179e1ebe9d9dc5bc099159341b3f
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-28 23:02:39 +0530

    24ansth commit

--- Array/Medium/FindtheDuplicateNumber.java
+++ Array/Medium/FindtheDuplicateNumber.java
@@ -8,5 +8,13 @@
 //        There is only one repeated number in nums, return this repeated number.
 //
 //        You must solve the problem without modifying the array nums and uses only constant extra space.
+Example 1:
+
+        Input: nums = [1,3,4,2,2]
+        Output: 2
+        Example 2:
+
+        Input: nums = [3,1,3,4,2]
+        Output: 3
 public class FindtheDuplicateNumber {
 }
