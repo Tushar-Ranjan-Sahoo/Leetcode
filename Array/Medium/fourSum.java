7bf394c0dbc0b68d6c2413d259ab8d8d7214a1db
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-29 22:10:39 +0530

    25ansth commit

--- Array/Medium/fourSum.java
+++ Array/Medium/fourSum.java
@@ -29,4 +29,38 @@
 //        -109 <= target <= 109
 
 public class fourSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length; // size of the array
+        Set<List<Integer>> set = new HashSet<>();
+
+        // checking all possible quadruplets:
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                Set<Long> hashset = new HashSet<>();
+                for (int k = j + 1; k < n; k++) {
+
+                    // taking bigger data type
+                    // to avoid integer overflow:
+                    long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    long fourth = target-sum;
+                    if(hashset.contains(fourth)){
+                        List<Integer>temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add((int)fourth);
+                        // we need to sort these value
+
+                        temp.sort(Integer::compareTo);
+                        set.add(temp);
+                    }
+                    hashset.add((long)nums[k]);
+                }
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(set);
+        return ans;
+    }
 }
