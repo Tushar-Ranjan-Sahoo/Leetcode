77a14e0bf610ad29908bead36298ca284079e687
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-06-30 09:39:55 +0530

    26ansth commit

--- Array/Medium/MaximumPointsYouCanObtainfromCards.java
+++ Array/Medium/MaximumPointsYouCanObtainfromCards.java
@@ -37,4 +37,19 @@
 //        1 <= k <= cardPoints.length
 
 public class MaximumPointsYouCanObtainfromCards {
+    class Solution {
+        public int maxScore(int[] cardPoints, int k) {
+            int total = 0 ;
+            for (int i =0 ; i<k ;i++){
+                total +=  cardPoints[i];
+
+            }
+            int best = total ;
+            for (int i = k-1,j=cardPoints.length -1; i>=0; i--,j--){
+                total += cardPoints[j]-cardPoints[i];
+                best = Math.max(best,total);
+            }
+            return best;
+        }
+    }
 }
